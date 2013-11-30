f488f14de59e2256566bbe3a6970291a19800cec
Piotr Krzeminski <pio.krzeminski@gmail.com>
2013-11-30 19:51:17 +0100

    converting future to observable implemented in ObservableEx

--- suggestions/src/main/scala/suggestions/observablex/ObservableEx.scala
+++ suggestions/src/main/scala/suggestions/observablex/ObservableEx.scala
@@ -6,8 +6,8 @@ import scala.util._
 import scala.util.Success
 import scala.util.Failure
 import java.lang.Throwable
-import rx.lang.scala.Observable
-import rx.lang.scala.Scheduler
+import rx.lang.scala._
+import rx.lang.scala.subjects._
 
 object ObservableEx {
 
@@ -17,6 +17,11 @@ object ObservableEx {
    * @param f future whose values end up in the resulting observable
    * @return an observable completed after producing the value of the future, or with an exception
    */
-  def apply[T](f: Future[T])(implicit execContext: ExecutionContext): Observable[T] = ???
+  def apply[T](f: Future[T])(implicit execContext: ExecutionContext): Observable[T] = {
+    val subject = ReplaySubject[T]()
+    f onSuccess { case result => subject.onNext(result); subject.onCompleted() }
+    f onFailure { case t => subject.onError(t) }
+    subject
+  }
 
 }
